2b68e9a423d51240c31ed9961be4f55342689544
joshi-swetam <123986118+joshi-swetam@users.noreply.github.com>
2023-06-01 21:02:08 -0400

    updated logic - defined url

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,39 +1,43 @@
-//  let newYorkCoords = [40.73, -74.0059];
-//  let mapZoomLevel = 12;
+let url = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json'
+let newYorkCoords = [40.73, -74.0059];
+let mapZoomLevel = 12;
+
 
 // Create the createMap function.
-function createMap(bikeStations){
+function createMap (bicycleStations) {
 
   // Create the tile layer that will be the background of our map.
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
 
-  
+
   // Create a baseMaps object to hold the lightmap layer.
   let baseMaps = {
-    "Street Map": streetmap
-  };
+    "Street Map" : street
+  }
+  
 
   // Create an overlayMaps object to hold the bikeStations layer.
   let overlayMaps = {
-    "Bike Stations": bikeStations
-  };
+    "Bicycle Stations" : bicycleStations
+ }
 
   // Create the map object with options.
-  let myMap = L.map("map", {
-    center: [40.73, -74.0059],
-    zoom: 12,
-    layers:[streetmap, bikeStations]
-  });
+  let myMap = L.map("map-id", {
+    center: newYorkCoords,
+    zoom: mapZoomLevel,
+    layers: [street, bicycleStations]
+  })
 
   // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-  L.control.L(baseMaps, overlayMaps, {
-    collapsed:false
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
   }).addTo(myMap)
 }
+
 // Create the createMarkers function.
-function createMarkers(){
+function createMarkers(response) {
 
   // Pull the "stations" property from response.data.
   let stations = response.data.stations
@@ -56,8 +60,8 @@ function createMarkers(){
   }
   // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
   createMap(L.layerGroup(bikeMarkers))
-  }
+}
 
 
-  // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
-  d3.json(url).then(createMarkers);
+// Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
+d3.json(url).then(createMarkers);
